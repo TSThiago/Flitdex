4023d271bb2067f8e1ca02065924d836fed67522
sidney alexandre <sidneymetalop@gmail.com>
2022-11-24 15:17:35 -0300

    Finalizando index e style. Falta ajustes na main.js.

---
+++ src/main.js
@@ -0,0 +1,32 @@
+const textButton = document.getElementById('editButton')
+const changedText = document.getElementById('text')
+
+if (window.localStorage.length === 0) {
+    function changeText(e) {
+        changedText.textContent = window.localStorage.getItem("NewText")
+    }
+
+    function inputNewText(e) {
+        e.target.value = prompt("insira o novo texto:")
+        window.localStorage.setItem("NewText", e.target.value)
+        changeText(e)
+    }
+
+    textButton.addEventListener('click', inputNewText)
+
+} else {
+    
+    changedText.textContent = window.localStorage.getItem("NewText")
+    function changeText(e) {
+        changedText.textContent = window.localStorage.getItem("NewText")
+    }
+
+    function inputNewText(e) {
+        e.target.value = prompt("insira o novo texto:")
+        window.localStorage.setItem("NewText", e.target.value)
+        changeText(e)
+    }
+
+    textButton.addEventListener('click', inputNewText)
+
+}
